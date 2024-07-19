7e0883bed051806f2a16f412cc537a81f40cb846
Ramakrishnanomula <n.ramakrishna@anarghyacomm.in>
2024-07-19 21:44:43 +0530

    commit

---
+++ CONFESSION1/src/main/java/com/cms/confession/Confession1Application.java
@@ -0,0 +1,12 @@
+package com.cms.confession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Confession1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Confession1Application.class, args);
+	}
+}
